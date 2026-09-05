2480459c59fdbf868c89233ec9486179cda6082a
Ilham <dgt.ilhammln@gmail.com>
2026-09-05 23:58:39 +0700

    Deploy: update terbaru

--- resources/views/bio/theme3.blade.php
+++ resources/views/bio/theme3.blade.php
@@ -561,7 +561,7 @@
         @if(!empty($profile->store_slug) || !empty($config['wa']) || !empty($config['ig']) || !empty($config['tiktok']) || !empty($config['youtube']))
             <div class="social-row fade-up" style="animation-delay:0.2s">
                 @if(!empty($profile->store_slug)) <a href="{{ route('store.show', $profile->store_slug) }}" target="_blank"
-                class="social-icon" title="Toko Online buyle.id"><i class="fas fa-store"></i></a> @endif
+                class="social-icon" title="Online Store buyle.id"><img src="{{ asset('favicon.png') }}" alt="Toko" style="width:1em;height:1em;object-fit:contain;display:inline-block;vertical-align:middle;"></a> @endif
                 @if(!empty($config['wa'])) <a href="https://wa.me/{{ $config['wa'] }}" target="_blank"
                 class="social-icon" title="WhatsApp"><i class="fab fa-whatsapp"></i></a> @endif
                 @if(!empty($config['ig'])) <a href="https://instagram.com/{{ ltrim($config['ig'], '@') }}" target="_blank"
