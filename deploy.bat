0ec141b789a34d076afc04336bdfbe3c97429516
Ilham <dgt.ilhammln@gmail.com>
2026-09-06 20:11:51 +0700

    Deploy: update terbaru

--- deploy.bat
+++ deploy.bat
@@ -15,11 +15,10 @@ echo.
 echo ==============================================
 echo 3. Pull ke Hosting, Jalankan Migration ^& Clear Cache...
 echo ==============================================
-ssh -p 65002 u664715641@46.202.186.86 "cd domains/buyle.id/public_html && git checkout . && git pull origin main && php artisan storage:link && php artisan migrate --force && php artisan favicon:refresh && php artisan optimize:clear"
+ssh -p 65002 u664715641@46.202.186.86 "cd domains/buyle.id/public_html && rm -rf public_html/storage public/storage && git checkout . && git pull origin main && php artisan storage:link && php artisan migrate --force && php artisan favicon:refresh && php artisan optimize:clear"
 
 echo.
 echo ==============================================
 echo Deploy Berhasil!
 echo ==============================================
 pause
-
